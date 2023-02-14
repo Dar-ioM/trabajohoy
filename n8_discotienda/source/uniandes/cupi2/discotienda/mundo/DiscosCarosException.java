593ffc80c0c74eb64999a568f7002c5148075294
IVAN DARIO MARTINEZ <ivancchoo11@gmail.com>
2023-02-14 09:42:27 -0500

    inicio de repositorio

---
+++ n8_discotienda/source/uniandes/cupi2/discotienda/mundo/DiscosCarosException.java
@@ -0,0 +1,19 @@
+package uniandes.cupi2.discotienda.mundo;
+
+/**
+ * 
+ * @author JHONATAN
+ *
+ */
+public class DiscosCarosException extends Exception 
+{
+	/**
+	 * 
+	 * @param causa
+	 */
+	public DiscosCarosException(String causa)
+	{
+		super(causa);
+	}
+
+}
